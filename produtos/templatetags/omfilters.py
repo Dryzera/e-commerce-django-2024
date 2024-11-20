0d66220b382107b84d13510d158d30c0b983fcf3
Dryzera <endryusdaniel6321@gmail.com>
2024-11-20 13:19:14 -0300

    add cart

--- produtos/templatetags/omfilters.py
+++ produtos/templatetags/omfilters.py
@@ -1,7 +1,12 @@
 from django.template import Library
+from utils import utils
 
 register = Library()
 
 @register.filter
 def formata_preco(val):
     return f'R$ {val:.2f}'.replace('.', ',')
+
+@register.filter
+def cart_total_qtd(carrinho):
+    return utils.cart_total_qtd(carrinho)
--- produtos/urls.py
+++ produtos/urls.py
@@ -4,10 +4,10 @@ from . import views
 app_name = 'produto'
 
 urlpatterns = [
-    path('', views.ListaProdutos.as_view(), name='lista'),
-    path('<slug>/', views.DetalheProdutos.as_view(), name='detalhe'),
     path('adicionaraocarrinho/', views.AdicionarAoCarrinho.as_view(), name='adicionaraocarrinho'),
     path('removerdocarrinho/', views.RemoverDoCarrinho.as_view(), name='removerdocarrinho'),
-    path('carrinho/', views.Carrinho.as_view(), name='carinho'),
+    path('carrinho/', views.Carrinho.as_view(), name='carrinho'),
     path('finalizar/', views.Finalizar.as_view(), name='finalizar'),
+    path('<slug>/', views.DetalheProdutos.as_view(), name='detalhe'),
+    path('', views.ListaProdutos.as_view(), name='lista'),
 ]
