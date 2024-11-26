df167fe6334e73209cf1f9173d7421c5d00e9d89
Dryzera <endryusdaniel6321@gmail.com>
2024-11-26 06:39:29 -0300

    finishing e-commerce, add busca

--- produtos/urls.py
+++ produtos/urls.py
@@ -4,6 +4,7 @@ from . import views
 app_name = 'produto'
 
 urlpatterns = [
+    path('busca/', views.Busca.as_view(), name='busca'),
     path('adicionaraocarrinho/', views.AdicionarAoCarrinho.as_view(), name='adicionaraocarrinho'),
     path('removerdocarrinho/', views.RemoverDoCarrinho.as_view(), name='removerdocarrinho'),
     path('carrinho/', views.Carrinho.as_view(), name='carrinho'),
